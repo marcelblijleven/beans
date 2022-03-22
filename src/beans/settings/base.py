03e457cdf0f357928d9dba37a605711f9725b67a
Marcel Blijleven <marcelblijleven@gmail.com>
2022-03-22 11:53:07 +0100

    security: remove test secret key from base.py

--- src/beans/settings/base.py
+++ src/beans/settings/base.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-$!8x(!cho=0ec1)m%d$ae$=%9bv&4me12@^a5$vs8=t0+58!nq'
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
