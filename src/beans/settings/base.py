d4a77fbd1c44ebbfa1e9ec2f3ada2853635906f9
Marcel Blijleven <marcelblijleven@gmail.com>
2022-03-22 12:51:02 +0100

    chore: set correct auth user model

--- src/beans/settings/base.py
+++ src/beans/settings/base.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'beans.apps.base',
+    'beans.apps.coffee',
 ]
 
 MIDDLEWARE = [
@@ -92,7 +93,7 @@ DATABASES = {
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
 
-AUTH_USER_MODEL = 'beans.apps.base.models.User'
+AUTH_USER_MODEL = 'base.User'
 
 AUTH_PASSWORD_VALIDATORS = [
     {
