012dfbee975af2bed7f0156db5ba25c434eab019
Marcel Blijleven <marcelblijleven@gmail.com>
2022-03-22 11:46:40 +0100

    feat: initial project setup

---
+++ README.md
@@ -0,0 +1,13 @@
+# Beans
+A logging tool for specialty coffee ☕️
+
+## Get started
+```bash
+make install
+```
+
+To run locally for testing, run
+```bash
+python manage.py migrate
+python manage.py runserver
+```
---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import find_packages, setup
+
+# Get README content
+try:
+    with open("README.md", "r") as f:
+        readme_description = "\n".join(f.readlines())
+except IOError:
+    readme_description = ""
+
+setup(
+    name="beans",
+    version="",
+    author="Marcel Blijleven",
+    url="https://github.com/marcelblijleven/beans",
+    description="",
+    long_description=readme_description,
+    zip_safe=False,
+    install_requires=[],
+    extras_require={},
+    setup_requires=["setuptools_scm==3.1.0"],
+    package_dir={"": "src"},
+    packages=find_packages("src"),
+    include_package_data=True,
+    scripts=["manage.py"],
+)
