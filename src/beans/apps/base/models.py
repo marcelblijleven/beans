518a2372e7481e4126b0a84e5154a0e33a0da989
Marcel Blijleven <marcelblijleven@gmail.com>
2022-03-22 12:49:29 +0100

    feat: add user model

--- src/beans/apps/base/models.py
+++ src/beans/apps/base/models.py
@@ -1,3 +1,9 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class User(AbstractUser):
+    name = models.CharField(max_length=200, null=True)
+    email = models.EmailField(unique=True, null=False)
+
+    USERNAME_FIELD = 'email'
