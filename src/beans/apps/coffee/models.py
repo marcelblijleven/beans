b8a758f6a21b46d6ee5b4bf19fa3be5270706893
Marcel Blijleven <marcelblijleven@gmail.com>
2022-03-22 13:05:22 +0100

    feat: create model OriginCountry

--- src/beans/apps/coffee/models.py
+++ src/beans/apps/coffee/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
+class OriginCountry(models.Model):
+    class Continent(models.TextChoices):
+        AFRICA = "AF", _("Africa")
+        ANTARCTICA = "AN", _("Antarctica")
+        ASIA = "AS", _("Asia")
+        OCEANIA = "OC", _("Oceania")
+        EUROPE = "EU", _("Europe")
+        NORTH_AMERICA = "NA", _("North America")
+        SOUTH_AMERICA = "SA", _("South America")
+
+    name = models.CharField(max_length=80, unique=True)
+    continent = models.CharField(
+        max_length=2,
+        choices=Continent.choices
+    )
