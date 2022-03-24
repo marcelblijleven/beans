1344bc4f4813f4d85235c196b7ebf433d376e5a6
Marcel Blijleven <marcelblijleven@gmail.com>
2022-03-24 21:51:02 +0100

    chore: add roaster list url

--- src/beans/apps/coffee/urls.py
+++ src/beans/apps/coffee/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from beans.apps.coffee.views import add_coffee_view, coffee_detail_view, coffee_list_view
+from beans.apps.coffee.views import add_coffee_view, coffee_detail_view, coffee_list_view, roaster_list_view
 
 urlpatterns = [
     path("", coffee_list_view, name="coffee-list"),
     path("<int:pk>", coffee_detail_view, name="coffee-detail"),
     path("add-coffee/", add_coffee_view, name="add-coffee"),
+    path("roasters/", roaster_list_view, name="roaster-list"),
 ]
